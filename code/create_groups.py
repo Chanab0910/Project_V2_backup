593de22fe219e26f128d8215b5c356f68e6a9e84
Chanab0910 <chanab0910@highgateschool.org.uk>
2023-10-01 16:28:51 +0100

    got rid of repeating variables

--- code/create_groups.py
+++ code/create_groups.py
@@ -1,4 +1,3 @@
-import read_from_db
 from random import randint, shuffle, sample
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -6,20 +5,21 @@ from sqlalchemy.orm import Session
 
 class GroupGenerator:
     def __init__(self):
+        self.engine = self.create_engine('sqlite:///World_cup.sqlite3', echo=True)
+
+        self.sess = self.Session(self.engine)
         self.team = ''
         self.groups = [[] for i in range(8)]
-        self.countries_in_tiers = []
-        self.countries_in_tiers.append(sample(read_from_db.tier1_countries, len(read_from_db.tier1_countries)))
-        self.countries_in_tiers.append(sample(read_from_db.tier2_countries, len(read_from_db.tier2_countries)))
-        self.countries_in_tiers.append(sample(read_from_db.tier3_countries, len(read_from_db.tier3_countries)))
-        self.countries_in_tiers.append(sample(read_from_db.tier4_countries, len(read_from_db.tier4_countries)))
+        self.country = self.sess.query(self.Country).first()
+
+        self.countries = [sample(self.sess.query(self.Country).filter_by(tier=i).all()) for i in range(4)]
 
     def group_draw(self):
         group = []
         for i, group in enumerate(self.groups):
-            self.team = self.countries_in_tiers[i][randint(0, len(self.countries_in_tiers[i]))]
+            self.team = self.countries[i][randint(0, len(self.countries[i]))]
             group.append(self.team)
-            self.countries_in_tiers[i].remove(self.team)
+            self.countries[i].remove(self.team)
         return group
 
 
