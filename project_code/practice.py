27e307526c5eb968f7d3f8847c9f1eb71292ee2f
chanab0910 <chanab0910@highgateschool.org.uk>
2023-12-08 10:27:33 +0000

    UPDATE WORKS

--- project_code/practice.py
+++ project_code/practice.py
@@ -11,4 +11,22 @@ sess = Session(engine)
 
 conn = engine.connect()
 
+match_id = 3
+teams_id = [13, 12]
 
+score = (4, 3)
+
+country_matches = sess.query(CountryMatch).filter_by(match_id=4).all()
+
+country_matches[0].score = score[0]
+country_matches[1].score = score[1]
+
+if score[0] > score[1]:
+    country_matches[0].result = 'win'
+    country_matches[1].result = 'loss'
+elif score[1] > score[0]:
+    country_matches[0].result = 'loss'
+    country_matches[1].result = 'win'
+else:
+    country_matches[0].result = 'loss'
+    country_matches[1].result = 'win'
